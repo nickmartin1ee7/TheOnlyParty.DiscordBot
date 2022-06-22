c4a0bf0de25941af91723ab5f0212b1e0b3cc171
Nicholas M Martin <nmmart04@louisville.edu>
2022-06-22 18:17:02 -0400

    Add project files.

---
+++ TheOnlyParty.DiscordBot/Program.cs
@@ -0,0 +1,10 @@
+using TheOnlyParty.DiscordBot;
+
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services =>
+    {
+        services.AddHostedService<Worker>();
+    })
+    .Build();
+
+await host.RunAsync();
