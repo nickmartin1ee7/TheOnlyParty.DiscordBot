7d10e3df349b659b42c89ea08f5fed9a3b07cc8c
Nicholas M Martin <nmmart04@louisville.edu>
2022-07-02 14:28:22 -0400

    Added webapi

---
+++ TextSentiment.WebApi/Program.cs
@@ -0,0 +1,23 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.MapPost("/analyze", async () =>
+{
+    
+})
+.WithName("PostAnalyze");
+
+app.Run();
