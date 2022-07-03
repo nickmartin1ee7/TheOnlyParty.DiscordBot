1dffe398cb7625b9f661a817eb6ff14ee6eabaec
Nicholas M Martin <nmmart04@louisville.edu>
2022-07-03 16:20:02 -0400

    Parama disable

--- TheOnlyParty.DiscordBot/Models/UserOptStatus.cs
+++ TheOnlyParty.DiscordBot/Models/UserOptStatus.cs
@@ -5,6 +5,8 @@ namespace TheOnlyParty.DiscordBot.Models;
 public record UserOptStatus
 {
     [Key]
+#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
     public string UserId { get; set; }
+#pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
     public bool Enabled { get; set; }
 }
